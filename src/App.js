1e7c3511d26830a9068201318ca527cf647dfe49
Frederico Soares <soaresfred89@gmail.com>
2019-12-22 15:26:13 -0100

    styling home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import GlobalStyle from './styles/gobal';
+import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
 import Routes from './routes';
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,108 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito bonito</strong>
+        <span>$70</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff " /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }

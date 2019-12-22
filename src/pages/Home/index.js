bb2ef4d2d114fdc78c21de0b03d6a8837b377542
Frederico Soares <soaresfred89@gmail.com>
2019-12-22 22:06:12 -0100

    load products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,108 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff " /> 3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg"
-          alt="Tênis"
-        />
-        <strong>Tênis muito bonito</strong>
-        <span>$70</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff " /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }

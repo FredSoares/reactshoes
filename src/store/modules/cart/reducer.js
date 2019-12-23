37b00cf7c4d742bdf5a12ec7f14c85decfe5bf46
Frederico Soares <soaresfred89@gmail.com>
2019-12-23 21:17:07 -0100

    update amount

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -23,6 +23,18 @@ export default function cart(state = [], action) {
           draft.splice(produtIndex, 1);
         }
       });
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return state;
+      }
+      return produce(state, draft => {
+        const produtIndex = draft.findIndex(p => p.id === action.id);
+
+        if (produtIndex >= 0) {
+          draft[produtIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }

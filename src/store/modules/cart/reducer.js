57b56c253cc45f47f6e747802b1ac29518770b6e
Frederico Soares <soaresfred89@gmail.com>
2019-12-24 14:02:22 -0100

    separating actions

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,16 +4,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const produtIndex = draft.findIndex(p => p.id === action.product.id);
+        const { product } = action;
 
-        if (produtIndex >= 0) {
-          draft[produtIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draft.push(product);
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
